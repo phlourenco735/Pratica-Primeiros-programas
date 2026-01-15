6780391055bd95409c982ad9b4720763f19a3b4a
Pedro Lourenço <61694156+phlourenco735@users.noreply.github.com>
2026-01-15 15:55:12 -0300

    Add files via upload

---
+++ adivinhe_o_numero.py
@@ -0,0 +1,13 @@
+import os
+
+os.system('cls')
+
+print("Pense em um número de 1 a 10...")
+input("Quando estiver pronto, aperte Enter.")
+
+os.system('cls')
+print("Estou lendo sua mente...")
+input()
+
+print("Seu número é... 7!")
+input("Acertei? 😄")
---
+++ pense_em_uma_cor.py
@@ -0,0 +1,17 @@
+import os
+
+os.system('cls')
+
+print("Escolha uma cor na sua mente:")
+print("🔴 Vermelho")
+print("🔵 Azul")
+print("🟢 Verde")
+print("🟡 Amarelo")
+input("Aperte Enter quando tiver escolhido...")
+
+os.system('cls')
+print("Concentre-se...")
+input()
+
+print("Você pensou na cor... 🔵 Azul!")
+input("Acertei, né?")
